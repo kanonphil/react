12263a07a6afd9774a8b53048ea3fb64d707ef3a
kanonphil <xoghkdbwjd@gmail.com>
2026-01-26 16:26:53 +0900

    delete

---
+++ redux-example/src/store.js
@@ -0,0 +1,10 @@
+import counterReducer from './reducers/counterReducer'
+import { configureStore } from '@reduxjs/toolkit'
+
+const store = configureStore({
+  reducer: {
+    counter: counterReducer
+  }
+})
+
+export default store
