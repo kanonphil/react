b505e1e71325afe2e02cfd275e97b46e9a942579
kanonphil <xoghkdbwjd@gmail.com>
2026-02-05 09:49:02 +0900

    Learn functional updates in setState

--- front-board/src/App.jsx
+++ front-board/src/App.jsx
@@ -5,18 +5,27 @@ import RegForm from "./pages/Board/RegForm/RegForm"
 import BoardDetail from "./pages/Board/Detail/BoardDetail"
 import UpdateForm from "./pages/Board/UpdateForm/UpdateForm"
 import Header from "./components/Header/Header"
+import UseState1 from "./study/UseState1"
 
 function App() {
   return (
     <>
       <Header />
-      <Routes>
-        {/* 게시글 목록 페이지 */}
+
+      <div style={{
+        padding: '40px'
+      }}>
+        {/* 학습용 컴포넌트 */}
+        <UseState1 />
+      </div>
+      
+
+      {/* <Routes>
         <Route path="/" element={<BoardList />} />
         <Route path="/reg" element={<RegForm />} />
         <Route path="/detail/:boardNum" element={<BoardDetail />} />
         <Route path="/update/:boardNum" element={<UpdateForm />} />
-      </Routes>
+      </Routes> */}
     </>
   )
 }
---
+++ front-board/src/study/UseState1.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+
+// State 변경함수 코드가 해석되면 
+// State 변경함수의 실행은 나머지 코드 실행 완료 후 일괄 처리
+// State 변경함수가 여러 개 실행 되어도 1회만 리렌더링 한다.
+// 화살표 함수를 사용하면 여러 개 가능
+
+// State 변경함수 사용법은 두 가지가 존재
+// 1. setCnt(cnt 변수에 저장할 값)
+//    ex> setCnt(7) -> cnt값을 7로 변경
+// 2. 함수형 업데이트: setCnt((이전 cnt 값) => {return cnt 변수에 저장할 값})
+//    ex> setCnt(prev => {return 7})
+//    -> setCnt(prev => 7)
+const UseState1 = () => {
+  const [cnt, setCnt] = useState(0)
+
+  const [cnt2, setCnt2] = useState(0)
+
+  return (
+    <div>
+      <h3>cnt = {cnt}</h3>
+      <button 
+        type="button"
+        onClick={() => {
+          console.log(cnt)
+          setCnt(cnt + 1)
+          console.log(cnt)
+          setCnt(cnt + 1)
+          console.log(cnt)
+        }}
+      >
+        클릭
+      </button>
+
+      <h3>cnt2 = {cnt2}</h3>
+      <button 
+        type="button"
+        onClick={() => {
+          console.log(cnt2)
+          setCnt2(prev => prev + 1)
+          console.log(cnt2)
+          setCnt2(prev => prev + 1)
+          console.log(cnt2)
+        }}
+      >
+        클릭
+      </button>
+    </div>
+  )
+}
+
+export default UseState1
